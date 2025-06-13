f5518279eb9789cfbd8a15009f112f75838fb7e1
Vivek2122 <vpal21324@gmail.com>
2025-06-13 16:52:42 -0400

    Fix Tailwind content paths and build styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./Pages/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./Modals/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
