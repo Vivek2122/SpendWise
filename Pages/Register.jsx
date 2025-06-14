1143931e27e8f8349f5c2a1851d443cd6ae6095d
Vivek2122 <vpal21324@gmail.com>
2025-06-14 12:24:48 -0400

    final

--- Pages/Register.jsx
+++ Pages/Register.jsx
@@ -22,7 +22,7 @@ function Register() {
 	const handleSubmit = async function (e) {
 		e.preventDefault();
 		try {
-			const res = await axios.post(`${import.meta.env.VITE_BASE_URL}/`, formData, {
+			const res = await axios.post("https://coin-traq-api.onrender.com", formData, {
 				withCredentials: true,
 			});
 			if (res.status === 201) {
